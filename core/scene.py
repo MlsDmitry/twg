dcc85c5c76fe4573f79bb40641599631e1572a8a
mlsdmitry <43723140+MlsDmitry@users.noreply.github.com>
2022-07-07 19:03:42 +0300

    remove dead code

--- core/scene.py
+++ core/scene.py
@@ -34,57 +34,19 @@ class GameScene(ShowBase):
 
         # model manager
         self.model_manager = ModelManager(loader=loader)
-        # self.model_manager.loader = loader
+        # load models
         self.load_scene()
-        # self.player = loader.loadModel("egg-models/anime-girl.egg")
-        # self.player = loader.loadModel("egg-models/human.egg")
+
         self.player = Player()
         self.player.model.reparentTo(render)
-        # self.player.setPos(0, -1, 0)
-        # self.player.setScale(0.1)
-        # self.player.setP(self.player, 90)
-        # self.player.setH(self.player, 180)
-        
-        # min_v, max_v = self.player.getTightBounds()
-        # print(max_v - min_v)
-        # load the 3D models
-        # self.plane = self.loader.loadModel("egg-models/plane")
-        self.tex = self.loader.loadTexture("textures/LPC_Terrain/block_#.png", multiview=True)
-        # self.tex.setWrapU(Texture.WM_repeat)
-        # self.tex.setWrapV(Texture.WM_repeat)
-        # self.plane.setTexture(self.tex1)
 
-        # self.box = self.loader.loadModel("egg-models/box.egg")
-        # self.box.reparentTo(self.render)
-# create an empty node path
-        # self.my_map = self.render.attachNewNode("iso-map")
-        # fill the empty node path with grid tiles
+        # load texture for cubes
+        self.tex = self.loader.loadTexture("textures/LPC_Terrain/block_#.png", multiview=True)
+
         self.create_map(10, 10)
 
-        # self.cam.setPos(0, -100, 0)
-
-        # self.terrain = GeoMipTerrain("waves")
-        # self.terrain.setHeightfield("textures/waves5/199.png")
-        # self.terrain.setColorMap("textures/seamlessTextures/100_1374_seamless.JPG")
-        # self.terrain.getRoot().setSz(35)
-        # self.terrain.getRoot().reparentTo(render)
-        # self.terrain.generate()
-
-        # z = self.terrain.getElevation(256, 256) * 40
-        # self.cam.setPos(0, 0, z)
-
-        # self.terrain.setFocalPoint(self.cam)
-        # self.taskMgr.add(self.updateTerrain, "update terrain")
-
         self.accept("escape", exit)
-        # self.accept("w", self.move, ["up"])
-        # # self.accept("w-up", self.endWalk)
-        # self.accept("s", self.move, ["down"])
-        # # self.accept("s-up", self.endReverse)
-        # self.accept("a", self.move, ["left"])
-        # # self.accept("a-up", self.endTurnLeft)
-        # self.accept("d", self.move, ["right"])
-        # self.accept("d-up", self.endTurnRight)
+
         taskMgr.add(self.game_loop, 'game_loop')
 
 
