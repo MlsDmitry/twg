95e97d1ecc44e77f7f1980d49db2b814b13fb74b
mlsdmitry <43723140+MlsDmitry@users.noreply.github.com>
2022-07-08 11:02:56 +0300

    final model;

--- main.py
+++ main.py
@@ -5,12 +5,46 @@ game.run()
 """
 
 # from core import IsommtricGame
+import subprocess
+import sys
+
 import core
 
 
 def main():
+    # cmdstring = ('ffmpeg', # put it in the same dir
+    #         '-y',         # overwrite the file w/o warning
+    #         '-r', '%f' % 30.0, # frame rate of encoded video
+    #         '-an',       # no audio
+    #         '-analyzeduration', '0',  # skip auto codec analysis
+    #         # input params
+    #         '-s', '800x600', # default panda window size
+    #         '-f', 'rawvideo',   # RamImage buffer is raw buffer
+    #         '-pix_fmt', 'rgba', # format of panda texure RamImage buffer
+    #         '-i', '-',  # this means a pipe
+    #         # output params
+    #         # '-vtag', 'xvid', 
+    #         '-vcodec', 'mpeg4',
+    #         'screen.mp4')
+        
+    # p = subprocess.Popen(
+    #         cmdstring, 
+    #         stdin=subprocess.PIPE,
+    #         bufsize=-1, 
+    #         shell=False,
+    #         )
+    
     game = core.GameScene()
-    game.run()
+
+    if len(sys.argv) > 1:
+        game.record()
+    # game.proc = p
+    try:
+        game.run()
+    finally:
+        if len(sys.argv) > 1:
+            subprocess.run('ffmpeg -r 30 -f image2 -s 2880x1800 -i frame_%04d.png -vcodec libx264 -crf 25  -pix_fmt yuv420p screen.mp4 -y', shell=True)
+        subprocess.run('rm frame_*', shell=True)
     
 
 
